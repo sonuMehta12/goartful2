4dd1cf596ddc704be37571da7ad625e156c7e568
Sonu Kumar <sonu12022000@gmail.com>
2025-06-04 20:31:13 +0530

    facebook fixel is added

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import Navbar from "@/components/Navbar"; // <<< IMPORT THE NEW NAVBAR
 import Footer from "@/components/Footer"; // Your Footer component
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils"; // Utility function for classnames
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -27,6 +28,40 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              !function(f,b,e,v,n,t,s) {
+                if (f.fbq) return;
+                n=f.fbq=function(){
+                  n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                };
+                if (!f._fbq) f._fbq=n;
+                n.push=n;
+                n.loaded=!0;
+                n.version='2.0';
+                n.queue=[];
+                t=b.createElement(e);
+                t.async=!0;
+                t.src=v;
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1271271444528924');
+              fbq('track', 'PageView');
+            `,
+        }}
+      />
+      <noscript>
+        <Image
+          height="1"
+          width="1"
+          alt="Facebook Pixel"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1271271444528924&ev=PageView&noscript=1"
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",

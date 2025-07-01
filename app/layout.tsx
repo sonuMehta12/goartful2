b05029665cf719255297e1b1789cb40f8f6d429b
Sonu Kumar <sonu12022000@gmail.com>
2025-07-01 13:52:52 +0530

    attempt 3rd

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import Navbar from "@/components/Navbar"; // <<< IMPORT THE NEW NAVBAR
 import Footer from "@/components/Footer"; // Your Footer component
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils"; // Utility function for classnames
-import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -26,49 +25,43 @@ interface RootLayoutProps {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  const isProduction = process.env.NODE_ENV === 'production';
+  
   return (
     <html lang="en" suppressHydrationWarning>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-              !function(f,b,e,v,n,t,s) {
-                if (f.fbq) return;
-                n=f.fbq=function(){
-                  n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                };
-                if (!f._fbq) f._fbq=n;
-                n.push=n;
-                n.loaded=!0;
-                n.version='2.0';
-                n.queue=[];
-                t=b.createElement(e);
-                t.async=!0;
-                t.src=v;
-                s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)
-              }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1271271444528924');
-              fbq('track', 'PageView');
+      {/* Google Tag Manager - Only in Production */}
+      {isProduction && gtmId && (
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}');
             `,
-        }}
-      />
-      <noscript>
-        <Image
-          height="1"
-          width="1"
-          alt="Facebook Pixel"
-          className="hidden"
-          style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=1271271444528924&ev=PageView&noscript=1"
+          }}
         />
-      </noscript>
-      {/* End Meta Pixel Code */}
+      )}
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           inter.variable
         )}
       >
+        {/* Google Tag Manager (noscript) - Only in Production */}
+        {isProduction && gtmId && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
+        
         <ThemeProvider
           attribute="class"
           defaultTheme="system" // Or your preferred default
